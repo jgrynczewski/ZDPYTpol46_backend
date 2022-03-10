50ca03e709a90e24a7b8c9f3761cff937a94830b
jgrynczewski <Tel^)%322))$>
2022-03-10 21:02:41 +0100

    xss, render and templates

--- intro/views.py
+++ intro/views.py
@@ -1,6 +1,45 @@
+from markupsafe import escape
+
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 
+
 def hello(request):
     return HttpResponse("Hello, world!")
+
+
+def adam(request):
+    return HttpResponse("Cześć, Adam!")
+
+
+def ewa(request):
+    return HttpResponse("Cześć, Ewa!")
+
+
+def name_view(request, name):
+    msg = f"Cześć, {name}!"
+    sanitize_msg = escape(msg)
+    return HttpResponse(sanitize_msg)
+
+
+def hello2(request):
+    content = """
+    <!DOCUMENT html>
+        <html>
+            <head>
+                <body>
+                    <h1>Witaj, świecie</h1>
+                </body>
+            </head>
+        </html>
+    """
+
+    return HttpResponse(content)
+
+
+def hello3(request):
+    return render(
+        request,
+        'intro/hello.html'
+    )
