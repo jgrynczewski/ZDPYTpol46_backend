53e90d6034f77e9c3c5ba223d638d72236ffe5a4
jgrynczewski <Tel^)%322))$>
2022-03-10 21:02:24 +0100

    local urlconf

--- intro/urls.py
+++ intro/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from intro import views
 
+
 urlpatterns = [
     path('home/', views.hello),
+    path('home2/', views.hello2),
+    path('home3/', views.hello3),
+    path('adam/', views.adam),
+    path('ewa/', views.ewa),
+    path('<str:name>/', views.name_view),
 ]
