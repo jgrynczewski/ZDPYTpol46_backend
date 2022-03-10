ef2933fa6acad1e49058975bdf0ad4def6840f99
jgrynczewski <Tel^)%322))$>
2022-03-10 18:07:25 +0100

    first view

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.shortcuts import HttpResponse
+
+
+def hello(request):
+    return HttpResponse("Hello, world!")
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', hello),
 ]
