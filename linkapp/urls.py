4f0bf9b32c9d145431b97ff8383421073bd73b7e
jgrynczewski <Tel^)%322))$>
2022-03-22 20:45:49 +0100

    django 5 part 2

--- linkapp/urls.py
+++ linkapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from linkapp import views
 
+app_name = 'linkapp'
+
 urlpatterns = [
     path('first/', views.first, name='first'),
     path('second/', views.second, name='second'),
---
+++ mem/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+def mem(request):
+    return render(
+        request,
+        'mem/sample.html',
+    )
