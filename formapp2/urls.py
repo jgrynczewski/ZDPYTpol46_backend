f6f0045be7c3b1466aa6c0e3e38279668e072d84
jgrynczewski <Tel^)%322))$>
2022-03-31 21:07:37 +0200

    django 11 part 9

--- formapp2/urls.py
+++ formapp2/urls.py
@@ -6,5 +6,7 @@ app_name = "formapp2"
 
 urlpatterns = [
     path('list/', views.message_list, name='message-list'),
-    path('1/', views.contact1, name='contact1')
+    path('1/', views.contact1, name='contact1'),
+    path('2/', views.contact2, name='contact2'),
+    path('3/', views.contact3, name='contact3'),
 ]
--- formapp2/views.py
+++ formapp2/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from formapp2.models import Message
-
+from formapp2.forms import ContactForm, MessageForm
 
 def message_list(request):
     messages = Message.objects.all()
@@ -15,7 +15,7 @@ def message_list(request):
     )
 
 
-# Formularz HMTL
+# Formularz HTML
 def contact1(request):
     if request.method == "POST":
         data = request.POST
@@ -33,4 +33,54 @@ def contact1(request):
     return render(
         request,
         'formapp2/form1.html'
+    )
+
+
+# Formularz Django
+def contact2(request):
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)  # bound form
+
+        if form.is_valid():
+            data = form.cleaned_data
+
+            Message.objects.create(
+                name=data.get('name'),
+                email=data.get('email'),
+                category=data.get('category'),
+                subject=data.get('subject'),
+                body=data.get('body')
+            )
+
+        return redirect('formapp2:message-list')
+
+    form = ContactForm()  # unbound form
+    return render(
+        request,
+        'formapp2/form2.html',
+        context={
+            'form': form
+        }
+    )
+
+
+# Formularz modelu
+def contact3(request):
+
+    if request.method == "POST":
+        form = MessageForm(request.POST)  # bound form
+
+        if form.is_valid():
+            form.save()
+
+        return redirect('formapp2:message-list')
+
+    form = MessageForm()  # unbound form
+    return render(
+        request,
+        'formapp2/form3.html',
+        context={
+            'form': form
+        }
     )
