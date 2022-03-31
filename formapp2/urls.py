5a17842f5d1aa4a5b5b3545a18b93864c0c94b57
jgrynczewski <Tel^)%322))$>
2022-03-31 20:34:43 +0200

    django 11 part 8

--- formapp2/urls.py
+++ formapp2/urls.py
@@ -5,5 +5,6 @@ from formapp2 import views
 app_name = "formapp2"
 
 urlpatterns = [
+    path('list/', views.message_list, name='message-list'),
     path('1/', views.contact1, name='contact1')
 ]
--- formapp2/views.py
+++ formapp2/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from formapp2.models import Message
 
 
+def message_list(request):
+    messages = Message.objects.all()
+
+    return render(
+        request,
+        'formapp2/message_list.html',
+        context={
+            'messages': messages
+        }
+    )
+
+
+# Formularz HMTL
 def contact1(request):
+    if request.method == "POST":
+        data = request.POST
+
+        Message.objects.create(
+            name=data.get('name'),
+            email=data.get('email'),
+            category=data.get('category'),
+            subject=data.get('subject'),
+            body=data.get('body')
+        )
+
+        return redirect('formapp2:message-list')
+
     return render(
         request,
         'formapp2/form1.html'
