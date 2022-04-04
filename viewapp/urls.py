703d931d4a0dd08d2dac4cdb416cb97235506699
jgrynczewski <Tel^)%322))$>
2022-04-04 19:08:43 +0200

    django 12 part 2

--- viewapp/urls.py
+++ viewapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from viewapp import views
+from django.views.generic import TemplateView
 
 app_name = 'viewapp'
 
@@ -9,5 +10,15 @@ urlpatterns = [
     path('hello2/', views.HelloView.as_view(), name='hello2'),
 
     path('template/hello/', views.hello_template, name='hello-template'),
-    path('template/hello2/', views.HelloClassView.as_view(), name='hello-template2')
+    path('template/hello2/', views.HelloClassView.as_view(), name='hello-template2'),
+    path('template/hello3/', views.HelloTemplateView.as_view(), name='hello-template3'),
+    path(
+        'template/hello4/',
+        TemplateView.as_view(template_name="viewapp/hello.html"),
+        name='hello-template4'
+    ),
+
+    path('template2/hello/', views.hello_template2, name='hello2-template'),
+    path('template2/hello2/', views.HelloClassView2.as_view(), name='hello2-template2'),
+    path('template2/hello3/', views.HelloTemplateView2.as_view(), name='hello2-template3'),
 ]
--- viewapp/views.py
+++ viewapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from django import views
 
+from django.views.generic import TemplateView
+
 
 # function-based view
 def hello(request):
@@ -29,3 +31,43 @@ class HelloClassView(views.View):
             request,
             'viewapp/hello.html'
         )
+
+
+# generic view
+class HelloTemplateView(TemplateView):
+    template_name = 'viewapp/hello.html'
+
+
+# function-based view
+def hello_template2(request):
+    name = "Jan"
+    return render(
+        request,
+        'viewapp/hello2.html',
+        context={
+            "name": name,
+        }
+    )
+
+
+# class-based view
+class HelloClassView2(views.View):
+    def get(self, request):
+        name = "Jan"
+        return render(
+            request,
+            'viewapp/hello2.html',
+            context={
+                "name": name
+            }
+        )
+
+
+# generic view
+class HelloTemplateView2(TemplateView):
+    template_name = 'viewapp/hello2.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["name"] = "Jan"
+        return context
